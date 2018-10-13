309e6fad715bd776a3c443d093b894800072c5f4
Kunjung <kunjungsherpa@gmail.com>
2018-10-13 08:33:10 +0545

    changing sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,31 @@
+let data;
+let vectors;
+let pos;
+
+function preload() {
+	data = loadJSON('xkcd.json');
+	
+}
+
+function processData() {
+	let vectors = {};
+	let colors = data.colors;
+	for (let i = 0; i < colors.length; i++) {
+		let label = colors[i].color;
+		let rgb = color(colors[i].hex);
+		vectors[label] = createVector(red(rgb), green(rgb), blue(rgb));
+	}
+	return vectors;
+}
+
 function setup() {
 	noCanvas();
-	console.log('Hi Color vectors');
+	vectors = processData();
+	console.log(vectors);
+	pos = createVector(random(255), random(255), random(255));
+	
+	// console.log('Hey Color vectors');
+	// console.log(data)
 }
 
 function draw() {
